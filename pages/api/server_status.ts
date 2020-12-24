7b4ed92e4f82b7f04a3fc4b4ef0b794f77dff8f8
Ateniense <68018546+zeru2@users.noreply.github.com>
2020-12-24 15:19:41 -0300

    Update server_status.ts

--- pages/api/server_status.ts
+++ pages/api/server_status.ts
@@ -5,10 +5,8 @@ import DefserverStatus from '../../utils/StatusCheck'
 
 export default function Server(req: NextApiRequest, res: NextApiResponse) {
     try {
-        const sucessservermsg = "Server working properly"
-        DefserverStatus(sucessservermsg, 200, req, res)
+        DefserverStatus("Server working properly", 200, {}, req, res)
     } catch {
-        const errorservermsg = "Internal server error"
-        DefserverStatus(errorservermsg, 500, req, res)
+        DefserverStatus("Internal server error", 500, {}, req, res)
     }
 }
